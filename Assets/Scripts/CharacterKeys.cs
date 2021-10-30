38782fbce2392213a6b45b41e692529e549b197c
Jack Gagnon <jonathan.creed.gagnon@gmail.com>
2021-10-30 01:42:01 -0400

    Worked On Character Controller, needs to be redone

---
+++ Assets/Scripts/CharacterKeys.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterKeys : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public KeyCode interact;
+    public KeyCode click;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
